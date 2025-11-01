64cde0cd254694fcef09c4a665959505a834d69e
Sheetal Choure <sheetalchoure03@gmail.com>
2025-11-01 16:57:15 +0530

    backend

--- Backend/index.js
+++ Backend/index.js
@@ -34,11 +34,8 @@ app.get("/", (req, res) => {
 });
 
 // ✅ MongoDB connection
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+mongoose.connect(process.env.MONGO_URI)
+
   .then(() => console.log("✅ MongoDB connected"))
   .catch((err) => console.error("❌ MongoDB connection error:", err.message));
 
