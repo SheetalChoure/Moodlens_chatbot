7c10835c857753b8ff9bb643c8cf137e3be1b7fc
Sheetal Choure <sheetalchoure03@gmail.com>
2025-11-03 18:57:48 +0530

    backend

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,8 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "https://moodlenschatbot.netlify.app"
+    "https://moodlenschatbot.netlify.app",
+    "https://moodlens-chatbot.vercel.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
